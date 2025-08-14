6b515289c5b4651bc7fc60b8e663e469527a5717
Maximiliano Osorio <maxiosorio@gmail.com>
2025-08-14 10:55:44 -0400

    fix: remove conflicting add_public_directory call
    Only use add_resource for /potree/ asset URLs to avoid
    conflict between public directory and resource serving.

--- src/ckanext-potree/ckanext/potree/plugin.py
+++ src/ckanext-potree/ckanext/potree/plugin.py
@@ -11,7 +11,6 @@ class PotreePlugin(plugins.SingletonPlugin):
     # IConfigurer
     def update_config(self, config_):
         toolkit.add_template_directory(config_, 'templates')
-        toolkit.add_public_directory(config_, 'public')
         toolkit.add_resource('public', 'potree')
         toolkit.add_resource('assets', 'ckanext-potree')
 
